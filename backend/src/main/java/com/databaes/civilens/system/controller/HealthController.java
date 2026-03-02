0502de775a3aa2746c2e5fa76e2021c28fd3c8d0
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2026-03-02 15:06:39 +0530

    refactor: move HealthController to a new package

--- backend/src/main/java/com/databaes/civilens/system/controller/HealthController.java
+++ backend/src/main/java/com/databaes/civilens/system/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.databaes.civilens.persona.controller;
+package com.databaes.civilens.system.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
